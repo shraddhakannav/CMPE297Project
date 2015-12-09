b67c006964e95cf24d5141ce02da95573b1175d5
shraddhakannav <shraddhakannav@gmail.com>
2015-12-08 22:56:23 -0800

    Starting the iOS app

---
+++ CMPE297Health/PageContentViewController.swift
@@ -0,0 +1,48 @@
+//
+//  PageContentViewController.swift
+//  CMPE297Health
+//
+//  Created by Shraddha Kannav on 12/8/15.
+//  Copyright © 2015 Shraddha Kannav. All rights reserved.
+//
+
+import UIKit
+
+class PageContentViewController: UIViewController {
+
+    @IBOutlet weak var heading: UILabel!
+    @IBOutlet weak var bkImageView: UIImageView!
+    
+    var pageIndex: Int?
+    var titleText : String!
+    var imageName : String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.bkImageView.image = UIImage(named: imageName)
+        self.heading.text = self.titleText
+        self.heading.alpha = 0.1
+        UIView.animateWithDuration(1.0, animations: { () ->Void in
+            self.heading.alpha = 1.0
+        })
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- CMPE297Health/ViewController.swift
+++ CMPE297Health/ViewController.swift
@@ -19,7 +19,8 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBOutlet weak var startHealthyApp: UIButton!
+    
 }
 
